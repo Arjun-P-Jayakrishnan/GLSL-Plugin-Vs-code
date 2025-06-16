6a73a5fc5f868741027286ab158973979c169f8d
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2025-06-16 20:32:52 +0530

    :tada: startover

---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("GLSL ShaderLab extension is live");
+
+  let disposable = vscode.commands.registerCommand(
+    "glslShaderLab.showPreview",
+    () => {
+      const panel = vscode.window.createWebviewPanel(
+        "glslPreview",
+        "GLSL Live Preview",
+        vscode.ViewColumn.Beside,
+        { enableScripts: true }
+      );
+
+      panel.webview.html = getWebviewContent();
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(): string {
+  return `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+    </html>
+  `;
+}
+
+export function deactivate() {}
